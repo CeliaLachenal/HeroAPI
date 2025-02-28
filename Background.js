db04b61076dbd074494d76537021fe3e74f441b1
CeliaLachenal <59375489+CeliaLachenal@users.noreply.github.com>
2025-02-28 18:20:08 +0100

    ajout du Js qui gère l'animation du background

---
+++ Background.js
@@ -0,0 +1,47 @@
+// /* let today = new Date()
+// let Time = today.toLocaleTimeString("fr-FR") */
+// let index = 0;
+// let nextindex = 1
+// /* console.log(Time) */
+
+// let backgroundImages = document.getElementsByClassName("background").innerHTML;
+
+// let backgroundArray = ['images/1_rose.jpg', 'images/2_rouge.jpg', 'images/3_orange.jpg', 'images/4_orange_clair.jpg', 'images/5_jaune.jpg', 'images/6_vert.jpg', 'images/7_turquoise.jpg', 'images/8_bleu_clair.jpg', 'images/8_bleu_clair.jpg', 'images/10_violet.jpg', 'images/11_indigo.jpg'];
+
+// console.log()
+
+// switchBackground(backgroundArray)
+
+// // window.setInterval(function() {
+// //     switchBackground(backgroundArray)
+// // },5000)
+
+// function switchBackground(objet) {
+//   let backgroundspace= document.querySelector('.main');
+//   let background = backgroundspace.querySelector('img')
+//   background.src = objet[index];
+//   transition("#background");
+//   newbackground.src= objet[index+1];
+//   transition('#next_background')
+  
+//     Si index est egal ou superieur a la longueur du tableau, index = 0, sinon index ++
+//     if( index++ >= objet.length-1 ) {
+//         index = 0  
+//         nextindex = 0 
+//     }
+//     else {
+//         index++  
+//         nextindex++ 
+//     }
+    
+// }
+
+// function transition(id) {
+//     let cible = document.querySelector(id)
+//     let background = cible.classList
+//     background.remove("fond")
+//     background.add("fade") 
+// }
+
+
+
